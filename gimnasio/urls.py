3a24cfff75a165460acad8f21a128211573cfe26
Gabriela Loachamin <gabriela.loachamin4487-arch>
2026-06-23 17:51:28 -0500

    Fix: Corregir importación de include y configuración de archivos estáticos en settings

--- gimnasio/urls.py
+++ gimnasio/urls.py
@@ -15,10 +15,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include  # <-- Aquí agregamos ', include'
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',include('socios.urls')),
-
+    path('', include('socios.urls')),  # Ahora Django ya sabe qué significa include
 ]
