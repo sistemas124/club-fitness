a980c630e0eabc6255e8a2e06ed37598ac033d85
Daniel Chicaiza <jeniffer.chicaiza7566@utc.edu.ec>
2026-06-23 21:04:32 -0500

    Backend: Configuración de admin.py para registrar modelos de Socios y Evaluaciones

--- socios/admin.py
+++ socios/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Socio, EvaluacionMedica
 
-# Register your models here.
+# Registramos los modelos para poder verlos y probarlos en el navegador
+admin.site.register(Socio)
+admin.site.register(EvaluacionMedica)
